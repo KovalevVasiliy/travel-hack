11e08d3c29aaf9bf19758e7f8e2384ef4a70c492
Kovalev Vasiliy <minskij10@yandex.ru>
2021-03-27 18:14:15 +0300

    Update news endpoint

--- news-service/src/main.go
+++ news-service/src/main.go
@@ -1,27 +1,14 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
-type Result struct {
-	Result      string `json:"result"`
-	Description string `json:"news"`
-}
-
-func getNews(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	var result Result
-	result.Result = "OK"
-	result.Description = "Nice balls"
-	_ = json.NewEncoder(w).Encode(result)
-}
-
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/news", getNews).Methods("GET")
+	r.HandleFunc("news", GetNews).Methods("GET")
+	r.HandleFunc("news/{id}", GetNewsById).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
