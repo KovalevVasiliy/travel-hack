330b7baaaf8fc5797146189c35d8a8cb8594c0d4
Kovalev Vasiliy <minskij10@yandex.ru>
2021-03-28 06:49:30 +0300

    Pretty py

--- service/services/core/routes/__init__.py
+++ service/services/core/routes/__init__.py
@@ -4,5 +4,5 @@ from services.api import Api
 
 api: Api = Api()
 
-from .region import top_categories_by_region, generate_news  # isort:skip
+from .region import top_categories_by_region #, generate_news  # isort:skip
 from .planner import plan_trip
